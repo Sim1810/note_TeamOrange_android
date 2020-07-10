e4edc5b22cb5c9ecc18a365c7a9d789faa584cb9
kiranlongia <kiranlongia912@gmail.com>
2020-07-10 18:36:44 -0400

    save note

--- app/src/main/java/com/example/note_teamorange_android/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/note_teamorange_android/activities/CreateNoteActivity.java
@@ -2,13 +2,29 @@ package com.example.note_teamorange_android.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.note_teamorange_android.R;
+import com.example.note_teamorange_android.database.NotesDatabase;
+import com.example.note_teamorange_android.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle , inputNoteSubtitle, inputNoteText;
+    private TextView textDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +39,60 @@ public class CreateNoteActivity extends AppCompatActivity {
             }
         });
 
+        inputNoteTitle = findViewById(R.id.inptNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNote);
+        textDateTime = findViewById(R.id.textDateTime);
+
+        textDateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
+                .format(new Date())
+        );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
+    }
+    private void saveNote(){
+        if(inputNoteTitle.getText().toString().trim().isEmpty()){
+            Toast.makeText(this,"Note title can't be empty",Toast.LENGTH_SHORT).show();
+            return;
+        }else if(inputNoteSubtitle.getText().toString().trim().isEmpty()
+            && inputNoteText.getText().toString().trim().isEmpty()){
+            Toast.makeText(this,"Note description can't be empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //preparing note object to save in database
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitle(inputNoteSubtitle.getText().toString());
+        note.setNoteText(inputNoteText.getText().toString());
+        note.setDateTime(textDateTime.getText().toString());
+
+        //room doesnot allow database operation on the main thread .that's why we'r using asyn task to save note.
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
 
 
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NotesDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
 
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        }
 
-
-
-
+        new SaveNoteTask().execute();
     }
 }
